9c62ca91f9c53bf0d85923377a193672a7e40196
Murat Iskenov <morat.aisken@gmail.com>
2022-06-11 15:41:18 +0600

    block-info 50%

---
+++ src/components/BlockInfo/BlockInfo.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './info.css'
+export default function BlockInfo() {
+  return (
+    <div className='block'>
+        <div className="block-one">
+            <div className="green-first">
+
+            </div>
+            <div className="green-second">
+
+            </div>
+        </div>
+        <div className="block-two">
+            <div className="blue-first">
+
+            </div>
+            <div className="blue-second">
+
+            </div>
+            
+        </div>
+        <div className="block-three">
+            <div className="grey-first">
+
+            </div>
+            <div className="grey-second">
+
+            </div>
+        </div>
+     </div>
+  )
+}
